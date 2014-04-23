ce11f16af778906d6b101b14b954ed8ad635f604
JoshSolomon <solomon.josha@gmail.com>
2014-04-23 14:19:23 -0500

    Homework
    It works!

--- script.js
+++ script.js
@@ -1,3 +1,38 @@
 $(document).ready(function(){
 
+$('.cat').attr('src', cat.jpg);
+
+$('#cat1').click(function(){
+	console.log("You clicked on the first image!");
+	$('#cat').fadeOut(function(){;
+		$('#cat').attr('src', 'cat.jpg');
+	})
+	$('#cat').fadeIn();
+});
+
+$('#cat2').click(function(){
+	console.log('You clicked on the second image!');
+	$('#cat').fadeOut(function(){;
+		$('#cat').attr('src', 'cat-two.jpg');
+	})
+	$('#cat').fadeIn();
+});
+
+$('#cat3').click(function(){
+	console.log('You clicked on the third image!');
+	$('#cat').fadeOut(function(){;
+		$('#cat').attr('src', 'cat-three.jpg');
+	})
+	$('#cat').fadeIn();
+});
+
+$('#cat4').click(function(){
+	console.log('You get the idea...');
+	$('#cat').fadeOut(function(){;
+		$('#cat').attr('src', 'cat-four.jpg');
+	})
+	$('#cat').fadeIn();
+});
+
+
 });
