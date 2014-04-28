b9d45c6c00179e956038fc47e0ce54592b230732
kklai <kkrebeccalai@gmail.com>
2014-04-28 11:01:58 -0500

    working with thumbnails

--- script.js
+++ script.js
@@ -1,3 +1,36 @@
 $(document).ready(function(){
+  // use $(“#INSERT-YOUR-OWN-ID”).attr(“src”, imgFileName.jpg); to change the image after click
+
+	$('#one').click(function(){
+		console.log("You clicked on the first drunk cat!");
+		$('#cat').fadeOut(function(){
+			$('#cat').attr('src', 'cat.jpg');
+		});
+		$('#cat').fadeIn();
+	});
+
+	$('#two').click(function(){
+		console.log("Second drunk cat!");
+		$('#cat').fadeOut(function(){
+			$('#cat').attr('src', 'cat-two.jpg');
+		})
+		$('#cat').fadeIn();
+	});
+
+	$('#three').click(function(){
+		console.log("Third drunk kitty");
+		$('#cat').fadeOut(function(){
+			$('#cat').attr('src', 'cat-three.jpg');
+		})
+		$('#cat').fadeIn();
+	});
+
+	$('#four').click(function(){
+		console.log("Fourth scary tiger");
+		$('#cat').fadeOut(function(){
+			$('#cat').attr('src', 'cat-four.jpg');
+		})
+		$('#cat').fadeIn();
+	});
 
 });
