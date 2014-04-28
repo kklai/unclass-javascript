5a9d8e07673b2c3e5291468dd23a5ec3e737139b
SophiaBollag <sophiabollag@u.northwestern.edu>
2014-04-28 11:33:05 -0500

    Photo fades
    I hope I'm submitting this correctly?

--- script.js
+++ script.js
@@ -1,3 +1,41 @@
 $(document).ready(function(){
 
+$("#cat-thumb").click(function(){
+	$("#cat").fadeOut(function() {
+		$("#cat").attr("src","cat.jpg");
+		$("#cat").fadeIn();
+		});
+	console.log("You clicked on the first image.")
+})
+
+$("#cat-two").click(function(){
+	$("#cat").fadeOut(function() {
+		$("#cat").attr("src","cat-two.jpg");
+		$("#cat").fadeIn();
+		});
+	console.log("You clicked on the second image.")
+})
+
+$("#cat-three").click(function(){
+	$("#cat").fadeOut(function() {
+		$("#cat").attr("src","cat-three.jpg");
+		$("#cat").fadeIn();
+		});
+		console.log("You clicked on the third image.")
+
+})
+
+$("#cat-four").click(function(){
+	$("#cat").fadeOut(function() {
+		$("#cat").attr("src","cat-four.jpg");
+		$("#cat").fadeIn();
+		});
+		console.log("You clicked on the fourth image.")
+
+})
+
+;
+
+$("#photoID").attr("src",cat.jpg);
+
 });
